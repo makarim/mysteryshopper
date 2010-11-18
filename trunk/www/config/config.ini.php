b1f12916f02c8612494ede878b415628db6d708f
wendy.ewt@gmail.com <wendy.ewt@gmail.com@f254fcc8-e02d-11de-8dcd-9b777bee35ae>
2010-11-18 07:14:53 +0000

    将自动登出的时间改为3600s,即一小时

--- trunk/www/config/config.ini.php
+++ trunk/www/config/config.ini.php
@@ -8,38 +8,38 @@
 	define("KFL_DIR", APP_DIR."/KFL");
 	define("APP_TEMP_DIR", APP_DIR."/tmp/");
 
-	
+
 //////////////////////////////////////////////////////
 //				Website Settings	                //
 //////////////////////////////////////////////////////
-		
+
 	$GLOBALS ["gSiteInfo"] ["web_charset"] =  "UTF-8";
 	$GLOBALS ["gSiteInfo"] ["web_keyword"] =  "Xppass";
 	$GLOBALS ["gSiteInfo"] ["web_description"] =  "PHP开源 SSO系统--Xppass";
 	$GLOBALS ["gSiteInfo"] ["web_title"] =  "美国斯巴特市场咨询有限公司";
-	
+
 //////////////////////////////////////////////////////
 //				Email   Settings	                //
-//////////////////////////////////////////////////////		
-		
+//////////////////////////////////////////////////////
+
 	$GLOBALS ["gEmail"] ["smtp_host"] =  "smtp.163.com";
 	$GLOBALS ["gEmail"] ["smtp_port"] =  "25";
-	$GLOBALS ["gEmail"] ["smtp_account"] =  "xppass@163.com";
-	$GLOBALS ["gEmail"] ["smtp_pass"] =  "111111";
-	$GLOBALS ["gEmail"] ["smtp_from"] =  "xppass@163.com";
+	$GLOBALS ["gEmail"] ["smtp_account"] =  "wanting_e@163.com";
+	$GLOBALS ["gEmail"] ["smtp_pass"] =  "516518";
+	$GLOBALS ["gEmail"] ["smtp_from"] =  "wanting_e@163.com";
 	$GLOBALS ["gEmail"] ["pop3_host"] =  "pop3.163.com";
-	
+
 //////////////////////////////////////////////////////
 //				TimeZone   Settings	                //
-//////////////////////////////////////////////////////		
-		
+//////////////////////////////////////////////////////
+
 	date_default_timezone_set("Asia/Shanghai");
-	
+
 
 //////////////////////////////////////////////////////
 //				Memcached  Settings	                //
-//////////////////////////////////////////////////////		
-		
+//////////////////////////////////////////////////////
+
 	$GLOBALS ["gMemcacheServer"] ["192.168.1.5:11212"] = array (
   'mmhost' => '192.168.1.5',
   'mmport' => '11212',
@@ -52,21 +52,21 @@
   'mmhost' => '192.168.1.5',
   'mmport' => '11213',
 );
-	
+
 //////////////////////////////////////////////////////
 //				Packet   Settings	                //
-//////////////////////////////////////////////////////		
-		
+//////////////////////////////////////////////////////
+
 	$GLOBALS ["gPacket"] ["cacheOpen"] =  1;
 	$GLOBALS ["gPacket"] ["cacheStore"] =  "file";
 	$GLOBALS ["gPacket"] ["cacheTime"] =  3600;
 	$GLOBALS ["gPacket"] ["cacheDir"] =  APP_TEMP_DIR;
 	$GLOBALS ["gPacket"] ["cacheServer"] =  array($GLOBALS ["gMemcacheServer"]["192.168.1.5:11213"]);
-	
+
 //////////////////////////////////////////////////////
 //				PageCache  Settings	                //
-//////////////////////////////////////////////////////		
-		
+//////////////////////////////////////////////////////
+
 	$GLOBALS ["gPageCache"] ["index"] ["rulename"]=  "index";
 	$GLOBALS ["gPa、geCache"] ["index"] ["cachestore"]=  "file";
 	$GLOBALS ["gPageCache"] ["index"] ["cacheserver"]=  array($GLOBALS ["gMemcacheServer"]["192.168.1.5:11211"]);;
@@ -75,53 +75,53 @@
 	$GLOBALS ["gPageCache"] ["index"] ["compressed"]=  1;
 	$GLOBALS ["gPageCache"] ["index"] ["action"]=  "";
 	$GLOBALS ["gPageCache"] ["index"] ["view"]=  "";
-	
+
 //////////////////////////////////////////////////////
 //				Session   Settings	                //
-//////////////////////////////////////////////////////		
-		
+//////////////////////////////////////////////////////
+
 	$GLOBALS ["gSession"] ["sessionHandle"] =  "file";
-	$GLOBALS ["gSession"] ["lifeTime"] =  1440;
+	$GLOBALS ["gSession"] ["lifeTime"] =  3600;//网站的生存时间
 	$GLOBALS ["gSession"] ["database"] =  $GLOBALS ["gDataBase"]["db_setting.db3"];
 	$GLOBALS ["gSession"] ["memcached"] =  array($GLOBALS ["gMemcacheServer"]["192.168.1.5:11212"],
 $GLOBALS ["gMemcacheServer"]["192.168.1.5:11213"]);;
-	
+
 //////////////////////////////////////////////////////
 //				Log   Settings	                //
-//////////////////////////////////////////////////////		
-		
+//////////////////////////////////////////////////////
+
 	$GLOBALS ["gLog"] ["sendemail"] =  "1";
 	$GLOBALS ["gLog"] ["subject"] =  "应用错误报告";
 	$GLOBALS ["gLog"] ["receiver"] =  "kakapowu@gmail.com";
 	$GLOBALS ["gLog"] ["maxExecTime"] =  "2";
 	$GLOBALS ["gLog"] ["maxMemUsed"] =  "1048576";
-	
+
 	//$GLOBALS['gGroups'][0] = "Overview";
 	$GLOBALS['gGroups'][5] = "Detail";
 	$GLOBALS['gGroups'][2] = "Environment";
 	$GLOBALS['gGroups'][1] = "Service";
 	$GLOBALS['gGroups'][3] = "Product";
 	$GLOBALS['gGroups'][4] = "Summary";
-	
+
 	$GLOBALS['gGroupsCap']['Service']	 = "C";
 	$GLOBALS['gGroupsCap']['Environment'] = "B";
 	$GLOBALS['gGroupsCap']['Product']	 = "D";
 	$GLOBALS['gGroupsCap']['Summary']	 = "E";
 	$GLOBALS['gGroupsCap']['Detail']	 = "A";
-	
+
 	$GLOBALS['gTypes']['yesorno']  =1;
 	$GLOBALS['gTypes']['vote']  =2;
 	$GLOBALS['gTypes']['fillblank']  =3;
 	$GLOBALS['gTypes']['time']  =4;
-	
+
 	$GLOBALS['gIndustry'][1] = 'catering';
 	$GLOBALS['gIndustry'][2] = 'retail';
-	
+
 	$GLOBALS['gCompanytype'][1] = 'foreign';
 	$GLOBALS['gCompanytype'][2] = 'private';
 	$GLOBALS['gCompanytype'][3] = 'nation';
 	$GLOBALS['gCompanytype'][4] = 'partnership';
-	
+
 	$GLOBALS['gCompanycate'][1] = 'cafe';
 	$GLOBALS['gCompanycate'][2] = 'club';
 	$GLOBALS['gCompanycate'][3] = 'restaurant';
@@ -142,7 +142,7 @@ $GLOBALS ["gMemcacheServer"]["192.168.1.5:11213"]);;
 	$GLOBALS['gFoodcate'][14] = 'huoguo';
 	$GLOBALS['gFoodcate'][15] = 'bread';
 	$GLOBALS['gFoodcate'][16] = 'selfhelp_food';
-	
+
 	$GLOBALS['gCompanysize'][1] = '< 10';
 	$GLOBALS['gCompanysize'][2] = '10-20';
 	$GLOBALS['gCompanysize'][3] = '20-50';
@@ -150,5 +150,5 @@ $GLOBALS ["gMemcacheServer"]["192.168.1.5:11213"]);;
 	$GLOBALS['gCompanysize'][5] = '100-150';
 	$GLOBALS['gCompanysize'][6] = '150-500';
 	$GLOBALS['gCompanysize'][7] = '>500';
-	
+
 ?>
