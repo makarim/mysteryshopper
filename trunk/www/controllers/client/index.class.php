7ac3e65fe16ffd13ebdfccd660d51267319e1a49
wendy.ewt@gmail.com <wendy.ewt@gmail.com@f254fcc8-e02d-11de-8dcd-9b777bee35ae>
2011-07-28 09:46:20 +0000

    当JiaShanghai登录时，则被导航到homehotel主页

--- trunk/www/controllers/client/index.class.php
+++ trunk/www/controllers/client/index.class.php
@@ -6,15 +6,15 @@ class index{
 		global $tpl;
 		$this->tpl = $tpl;
 		if(isset($_SESSION['_XppassOnlineCorp'])){
-			$this->login_corp = $_SESSION['_XppassOnlineCorp'];	
+			$this->login_corp = $_SESSION['_XppassOnlineCorp'];
 		}
-		
-		
+
+
 		$this->tpl->assign('corp',$this->login_corp);
 	}
     function view_defaults(){
-		
-		$msg = '';	
+
+		$msg = '';
 		$_SESSION['_XppassSignKey'] = uniqid();
 		if($this->login_corp){
 			$msg = "Welcome ".$this->login_corp['c_name']."";
@@ -23,7 +23,7 @@ class index{
 		if(isset($_SESSION['pwd_error'])) {
 			$show_code = $_SESSION['pwd_error'];
 		}
-		
+
 		$this->tpl->assign("name","It's a demo.");
 		$this->tpl->assign("msg",$msg);
 		$this->tpl->assign ( 'show_code', $show_code );
@@ -31,7 +31,7 @@ class index{
 		$this->tpl->assign("scr",'index.php');
     }
     function view_faq(){
-    	
+
     }
     function view_login(){
 		$_SESSION['_XppassSignKey'] = uniqid();
@@ -43,55 +43,63 @@ class index{
 		$passwd = $_POST ['password'];
 		$sign = $_POST ['s'];
 		$msg ='';
-		
+
 		//signature
 		if($sign!=hmac($_SESSION['_XppassSignKey'],$passwd)){
-			$msg = array('s'=> 400,'m'=>lang('illegalsignon'),'d'=>'');				
+			$msg = array('s'=> 400,'m'=>lang('illegalsignon'),'d'=>'');
 			exit(json_output($msg));
 		}
 		include_once("CorporationModel.class.php");
 		$corporation = new CorporationModel();
 		$corp_info = $corporation->getCorporationByName($c_name);
-		
+
 		if ($corp_info) {
-		
+
 			if ($corp_info['c_password'] == md5(($passwd))) {
 				if ($corp_info ['c_state'] == 1) {
-								
+
 					//auto login
 					$_SESSION['_XppassOnlineCorp'] = $corp_info;
-					
+
 					$_SESSION['_XppassSignKey'] = '';
-				
-					$msg = array('s'=> 200,'m'=>"ok",'d'=>$GLOBALS ['gSiteInfo'] ['www_site_url']."/client.php/home");				
+
+					if($c_name == 'test'){
+					$msg = array('s'=> 200,'m'=>"ok",'d'=>$GLOBALS ['gSiteInfo'] ['www_site_url']."/client.php/hometotal");
 					exit(json_output($msg));
-									
+					}else if($c_name == 'JiaShanghai'){
+					$msg = array('s'=> 200,'m'=>"ok",'d'=>$GLOBALS ['gSiteInfo'] ['www_site_url']."/client.php/homehotel");
+					exit(json_output($msg));
+					}else{
+					$msg = array('s'=> 200,'m'=>"ok",'d'=>$GLOBALS ['gSiteInfo'] ['www_site_url']."/client.php/home");
+					exit(json_output($msg));
+					}
+
 				} else {
 					$msg = lang('userforbidden');
 				}
-				
+
 			}else{
 				$msg = lang('pwdwrong');
 			}
 		} else {
-				
+
 				$msg = lang('usernotexist');
-			
-		}	
-		$msg = array('s'=> 400,'m'=>$msg,'d'=>'');				
+
+		}
+		$msg = array('s'=> 400,'m'=>$msg,'d'=>'');
 		exit(json_output($msg));
-		 		
+
 	}
 	function view_logout() {
 		//echo COOKIE_DOMAIN;
 		setcookie ( 'Xppass_IC_CARD', '', time () - 3600, '/', COOKIE_DOMAIN );
-		
-		
-		unset($_SESSION['_XppassOnlineCorp']);		
-		
-		
+
+
+		unset($_SESSION['_XppassOnlineCorp']);
+
+
 		redirect($GLOBALS ['gSiteInfo'] ['www_site_url']."/client.php/index/login");
 	}
-	
+
 }
 ?>
