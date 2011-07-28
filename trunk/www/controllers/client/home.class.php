5df72d07437d9858150ec84cd227e9bed25ae9d6
wendy.ewt@gmail.com <wendy.ewt@gmail.com@f254fcc8-e02d-11de-8dcd-9b777bee35ae>
2011-07-28 02:32:18 +0000

    新增重置密码函数view_corpresetpwd()

--- trunk/www/controllers/client/home.class.php
+++ trunk/www/controllers/client/home.class.php
@@ -45,6 +45,48 @@ class home{
 		$this->tpl->assign("stores",$stores);
 		$this->tpl->assign("type",$type);
 	}
+	function view_corpresetpwd(){
+		$type = !empty($_GET['view'])?$_GET['view']:"corpresetpwd";
+		$this->tpl->assign("type",$type);
+	}
+	function op_corpresetpwd(){
+
+//		echo "<pre/>";
+//		print_r($this->login_corp);
+
+		if (empty ( $_POST ['oldpwd'] ) or empty ( $_POST ['newpwd1'] ) or empty ( $_POST ['newpwd2'] )) {
+			show_message_goback(lang('insertpwd'));
+		}
+
+		$new1 = $_POST ['newpwd1'];
+		$new2 = $_POST ['newpwd2'];
+		if (strlen ( $new1 ) < 6 or strlen ( $new2 ) < 6) {
+			show_message_goback(lang('pwdrule'));
+		}
+
+		if (trim ( $new1 ) != trim ( $new2 )) {
+			show_message_goback(lang('pwdnotsame'));;
+		}
+
+		include_once("CorporationModel.class.php");
+
+		$corpmod = new CorporationModel();
+		//$user_info = $passmod->getUserById($this->login_user['user_id'],$this->login_user['user']);
+
+		//print_r($user_info);
+		if ($this->login_corp['c_password'] == md5(md5($_POST ['oldpwd']))) {
+
+			if (false!=$corpmod->updatePwdByCorp ( $this->login_corp['c_id'], md5(md5($new1)) )) {
+				show_message_goback(lang('pwdreset'));
+			} else {
+				show_message_goback(lang('failture'));
+			}
+		} else {
+			show_message_goback(lang('pwdwrong'));
+		}
+
+	}
+
 	function view_corpstats(){
 		$type = !empty($_GET['view'])?$_GET['view']:"corp";
 		$sdate = !empty($_GET['sdate'])?$_GET['sdate']:"";
